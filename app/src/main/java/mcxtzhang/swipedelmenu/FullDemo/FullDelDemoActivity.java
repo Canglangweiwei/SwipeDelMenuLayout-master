728f180a1b82b2d1fc7834d5f4294be2ef98f0d5
Canglangweiwei <weiweistar@163.com>
2017-09-24 16:49:58 +0800

    Initial commit

---
+++ app/src/main/java/mcxtzhang/swipedelmenu/FullDemo/FullDelDemoActivity.java
@@ -0,0 +1,97 @@
+package mcxtzhang.swipedelmenu.FullDemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mcxtzhang.swipemenulib.SwipeMenuLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mcxtzhang.listswipemenudemo.R;
+import mcxtzhang.swipedelmenu.SwipeBean;
+
+/**
+ * 介绍：完整的删除Demo
+ * 作者：zhangxutong
+ * 邮箱：zhangxutong@imcoming.com
+ * 时间： 2016/9/12.
+ */
+@SuppressWarnings("ALL")
+public class FullDelDemoActivity extends Activity {
+
+    private RecyclerView mRv;
+    private FullDelDemoAdapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
+    private List<SwipeBean> mDatas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_del_demo);
+        mRv = (RecyclerView) findViewById(R.id.rv);
+
+        initDatas();
+        mAdapter = new FullDelDemoAdapter(this, mDatas);
+        mAdapter.setOnDelListener(new FullDelDemoAdapter.onSwipeListener() {
+
+            @Override
+            public void onDel(int pos) {
+                if (pos >= 0 && pos < mDatas.size()) {
+                    Toast.makeText(FullDelDemoActivity.this, "删除:" + pos, Toast.LENGTH_SHORT).show();
+                    mDatas.remove(pos);
+                    mAdapter.notifyItemRemoved(pos);//推荐用这个
+                    //如果删除时，不使用mAdapter.notifyItemRemoved(pos)，则删除没有动画效果，
+                    //且如果想让侧滑菜单同时关闭，需要同时调用 ((CstSwipeDelMenu) holder.itemView).quickClose();
+                    //mAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTop(int pos) {
+                if (pos > 0 && pos < mDatas.size()) {
+                    SwipeBean swipeBean = mDatas.get(pos);
+                    mDatas.remove(swipeBean);
+                    mAdapter.notifyItemInserted(0);
+                    mDatas.add(0, swipeBean);
+                    mAdapter.notifyItemRemoved(pos + 1);
+                    if (mLayoutManager.findFirstVisibleItemPosition() == 0) {
+                        mRv.scrollToPosition(0);
+                    }
+                    //notifyItemRangeChanged(0,holder.getAdapterPosition()+1);
+                }
+            }
+        });
+        mRv.setAdapter(mAdapter);
+        mRv.setLayoutManager(mLayoutManager = new GridLayoutManager(this, 2));
+
+        //6 2016 10 21 add , 增加viewChache 的 get()方法，
+        // 可以用在：当点击外部空白处时，关闭正在展开的侧滑菜单。我个人觉得意义不大，
+        mRv.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    SwipeMenuLayout viewCache = SwipeMenuLayout.getViewCache();
+                    if (null != viewCache) {
+                        viewCache.smoothClose();
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    private void initDatas() {
+        mDatas = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            mDatas.add(new SwipeBean("" + i));
+        }
+    }
+}
---
+++ app/src/main/java/mcxtzhang/swipedelmenu/ListViewDelDemoActivity.java
@@ -0,0 +1,64 @@
+package mcxtzhang.swipedelmenu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.mcxtzhang.commonadapter.lvgv.CommonAdapter;
+import com.mcxtzhang.commonadapter.lvgv.ViewHolder;
+import com.mcxtzhang.swipemenulib.SwipeMenuLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mcxtzhang.listswipemenudemo.R;
+
+@SuppressWarnings("ALL")
+public class ListViewDelDemoActivity extends AppCompatActivity {
+
+    private static final String TAG = "zxt";
+    private ListView mLv;
+    private List<SwipeBean> mDatas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLv = (ListView) findViewById(R.id.test);
+
+        initDatas();
+        mLv.setAdapter(new CommonAdapter<SwipeBean>(this, mDatas, R.layout./*item_swipe_menu*/item_cst_swipe) {
+            @Override
+            public void convert(final ViewHolder holder, SwipeBean swipeBean, final int position) {
+                //((CstSwipeDelMenu)holder.getConvertView()).setIos(false);//这句话关掉IOS阻塞式交互效果
+                holder.setText(R.id.content, swipeBean.name);
+                holder.setOnClickListener(R.id.content, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(ListViewDelDemoActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                holder.setOnClickListener(R.id.btnDelete, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(ListViewDelDemoActivity.this, "删除:" + position, Toast.LENGTH_SHORT).show();
+                        //在ListView里，点击侧滑菜单上的选项时，如果想让擦花菜单同时关闭，调用这句话
+                        ((SwipeMenuLayout) holder.getConvertView()).quickClose();
+                        mDatas.remove(position);
+                        notifyDataSetChanged();
+                    }
+                });
+            }
+        });
+    }
+
+    private void initDatas() {
+        mDatas = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            mDatas.add(new SwipeBean("" + i));
+        }
+    }
+}
